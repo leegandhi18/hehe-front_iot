9b1a70d4b28862d418ae85064f81f0f14022ee04
juhyunju <2juhyunju@naver.com>
2022-03-08 01:24:43 +0900

    feat:header bar

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div style="margin-top: 80px">
+      <b-row align-h="center">
+        <b-col cols="4">
+          <b-card title="로그인">
+            <b-form-group label-cols="4" label-cols-lg="3" label="아이디" label-for="input-userid">
+              <b-form-input id="input-userid" v-model="userid"></b-form-input>
+            </b-form-group>
+            <b-form-group label-cols="4" label-cols-lg="3" label="패스워드" label-for="input-password">
+              <b-form-input id="input-password" v-model="password" type="password"></b-form-input>
+            </b-form-group>
+            <b-form-group label-cols="4" label-cols-lg="3" label="">
+              <b-button id="login" variant="dark" :disabled="loading" @click="onSubmit"
+                ><b-spinner v-if="loading" small></b-spinner> 로그인</b-button
+              >
+            </b-form-group>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style>
+#login {
+  float: right;
+}
+</style>
