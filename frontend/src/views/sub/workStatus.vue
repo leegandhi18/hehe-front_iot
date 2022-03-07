9f1aeb0d0d9e1aa3fa096a0f852fb05576ad9b93
juhyunju <2juhyunju@naver.com>
2022-03-08 03:52:39 +0900

    feat:workStatus add button

--- frontend/src/views/sub/workStatus.vue
+++ frontend/src/views/sub/workStatus.vue
@@ -8,11 +8,9 @@
     </b-col>
     <div>
       <b-table small hover striped :items="items" :fields="fields">
-        <template #cell(updateBtn)="row">
-          <b-button size="sm" variant="success" @click="onClickEdit(row.item.id)">수정</b-button>
-        </template>
-        <template #cell(deleteBtn)="row">
-          <b-button size="sm" variant="danger" @click="onClickDelete(row.item.id)">삭제</b-button>
+        <template #cell(btn)="row">
+          <b-button size="sm" variant="dark" @click="onClickEdit(row.item.id)">수정</b-button>
+          <b-button size="sm" variant="dark" @click="onClickDelete(row.item.id)">삭제</b-button>
         </template>
       </b-table>
     </div>
@@ -24,35 +22,42 @@ export default {
   data() {
     return {
       // Note `isActive` is left out and will not appear in the rendered table
-      fields: ['first_name', 'last_name', 'age', 'test'],
+      fields: [
+        { key: 'id' },
+        { key: 'name', label: '이름' },
+        { key: 'last_name', label: '몰라' },
+        { key: 'age', label: '나이' },
+        { key: 'btn', label: '비고' }
+        // { key: 'deleteBtn', label: '삭제' }
+      ],
       items: [
         {
           id: '1',
           first_name: 'sfdsa',
           last_name: 'dfs',
           age: '30',
-          test: ''
+          btn: ''
         },
         {
           id: '2',
           first_name: 'sfdsa',
           last_name: 'dfs',
           age: '30',
-          test: ''
+          btn: ''
         },
         {
           id: '3',
           first_name: 'sfdsa',
           last_name: 'dfs',
           age: '30',
-          test: ''
+          btn: ''
         },
         {
           id: '4',
           first_name: 'sfdsa',
           last_name: 'dfs',
           age: '30',
-          test: ''
+          btn: ''
         }
       ]
     }
