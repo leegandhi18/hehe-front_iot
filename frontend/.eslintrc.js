ec6cf8c61227e8ccb2c55abe7e2dc50b09a4297b
leegandhi18 <leegandhi18@gmail.com>
2022-04-01 15:32:56 +0900

    feat: add grafana dashboard

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,31 +1,29 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-	},
-	extends: [
-		'plugin:vue/essential',
-		'eslint:recommended',
-		'plugin:prettier/recommended',
-	],
-	parserOptions: {
-		parser: '@babel/eslint-parser',
-	},
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'prettier/prettier': [
-			'error',
-			{
-				singleQuote: true,
-				semi: true,
-				useTabs: true,
-				tabWidth: 2,
-				trailingComma: 'all',
-				printWidth: 80,
-				bracketSpacing: true,
-				arrowParens: 'avoid',
-			},
-		],
-	},
+  root: true,
+  env: {
+    node: true,
+  },
+  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "prettier/prettier": [
+      "error", {
+        singleQuote: true,
+        semi: false,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: "none",
+        printWidth: 120,
+        bracketSpacing: true,
+        arrowParens: "avoid",
+        endOfLine: "auto",
+      }
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": ["warn", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
+  },
 };
--- frontend/src/views/dashBoard/index.vue
+++ frontend/src/views/dashBoard/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <edukit />
+    <iframe
+      src="http://localhost:3005/d-solo/0e8C2fsnk/hehe?orgId=1&from=1648766307184&to=1648787907184&panelId=2"
+      width="450"
+      height="200"
+      frameborder="0"
+    ></iframe>
   </div>
 </template>
 
